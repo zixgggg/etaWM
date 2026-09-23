c574b4af02dbd27100cf40648a20c8fc67abec5e
zixgggg <zixzix035@gmail.com>
2026-09-23 18:07:17 +0800

    can move and resize window now

--- main.c
+++ main.c
@@ -23,14 +23,14 @@ int main(){
     
 	XDefineCursor(dpy,DefaultRootWindow(dpy),cursor);
 	
-	int mc_origin_x;
+	int mc_origin_x;//滑鼠原本的xy
 	int mc_origin_y;
-	int win_origin_x;
+	int win_origin_x;//視窗原本的xy
 	int win_origin_y;
-	int win_origin_width;
-	int win_origin_height;
+	unsigned int win_origin_width;//視窗原本的寬高，XGetGeometry裡面的width_return,height_return等都是unsigned int
+	unsigned int win_origin_height;
 	Window root;
-	int border,depth;
+	unsigned int border,depth;
 	XButtonEvent start;
 	start.subwindow=None;
 	while(True){
@@ -49,44 +49,38 @@ int main(){
 		else if(ev.type==ButtonPress && ev.xbutton.subwindow!=None){
 			start=ev.xbutton;
 			XGetGeometry(dpy,ev.xbutton.subwindow,&root,&win_origin_x,&win_origin_y,&win_origin_width,&win_origin_height,&border,&depth);
-			/*
-			int mc_origin_x=ev.xbutton.x_root;
-			int mc_origin_y=ev.xbutton.y_root;
-			if(start.button==1){
-			}
-			else if(start.button==3){
-				win_origin_width=ev.xbutton.subwindow.XGetGeometry.width_return;
-				win_origin_height=ev.xbutton.subwindow.XGetGeometry.height_return;
-			}
-			*/
+			mc_origin_x=ev.xbutton.x_root;
+			mc_origin_y=ev.xbutton.y_root;
 		}
-		else if(ev.type==MotionNotify && start!=None){
-			int moved_x=ev.xbutton.x_root;
+		else if(ev.type==MotionNotify && start.subwindow!=None){
+			int moved_x=ev.xbutton.x_root;//滑鼠移動後的xy
 			int moved_y=ev.xbutton.y_root;
 
-			int move_x=moved_x-mc_origin_x;
+			int move_x=moved_x-mc_origin_x;//滑鼠了xy移動多少
 			int move_y=moved_y-mc_origin_y;
 
 			if(start.button==1){
 				XMoveWindow(dpy,start.subwindow,win_origin_x+move_x,win_origin_y+move_y);
 			}
-			else if(start.subwindow==3){
+			else if(start.button==3){
+	           //最小視窗寬高常數（最小可設定為1）
 				const int MIN_WIN_WIDTH=50;
 				const int MIN_WIN_HEIGHT=50;
 				
 				XResizeWindow(dpy,start.subwindow,
-					MAX(win_origin_width+move_x,MIN_WIN_WIDTH),
-					MAX(win_origin_height+move_y,MIN_WIN_HEIGHT)
+					MAX((int)win_origin_width+move_x,MIN_WIN_WIDTH),//視窗原本的位置+滑鼠移動了多少（也就是要移動多少），unsigned int強制轉型成(int) 因為win_origin_width跟win_origin_height是unsigned int，跟int計算會溢位
+					MAX((int)win_origin_height+move_y,MIN_WIN_HEIGHT)
 				);
 			}
 		}
 		else if(ev.type==ButtonRelease && ev.xbutton.subwindow!=None){
-			start=None;
+			start.subwindow=None;
 		}
 		
-		else if(ev.type==EnterNotify && ev.xbutton.subwindow!=None){
-			XRaiseWindow(dpy,ev.xbutton.subwindow);
+		else if(ev.type==EnterNotify && ev.xcrossing.subwindow!=None){
+			XRaiseWindow(dpy,ev.xcrossing.subwindow);
 		}
+		XFlush(dpy);
 	}
 	return 0;
 }
