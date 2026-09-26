6980c865d86854c8e030cbca4e3db33784dc7709
zixgggg <zixzix035@gmail.com>
2026-09-26 15:26:33 +0800

    kill window by focus,focus and unfocus border color change,add DestroyNotify

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <X11/Xlib.h>
 #include <X11/cursorfont.h>
 /*
+need to install libx11-dev
 header at  /usr/include/X11/Xlib.h
 install libx11-doc can see man page
 API reference:https://tronche.com/gui/x/xlib/function-index.html
@@ -9,8 +10,21 @@ event structures:https://tronche.com/gui/x/xlib/events/structures.html
 */
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
+Display * dpy;
+XColor focus_border_color,exact,unfocus_border_color;
+const int BORDER_WIDTH=3;
+Window focused_win=None;
+void focus_win_func(Window w){
+	XSetInputFocus(dpy,w,RevertToParent,CurrentTime);
+	XRaiseWindow(dpy,w);
+	XSetWindowBorder(dpy,w,focus_border_color.pixel);
+	XSetWindowBorderWidth(dpy,w,BORDER_WIDTH);
+	if(focused_win!=None && focused_win!=w){
+		XSetWindowBorder(dpy,focused_win,unfocus_border_color.pixel);
+	}
+	focused_win=w;
+}
 int main(){
-	Display * dpy;
 	dpy=XOpenDisplay(NULL);
 	XEvent ev;
 	Cursor cursor=XCreateFontCursor(dpy,XC_left_ptr);
@@ -37,21 +51,15 @@ int main(){
 	unsigned int border,depth;
 	XButtonEvent start;
 	start.subwindow=None;
-	XColor border_color,exact;
-	XAllocNamedColor(dpy,DefaultColormap(dpy,DefaultScreen(dpy)),"blue",&border_color,&exact);//color_name at /usr/share/X11/rgb.txt,or you can see https://en.wikipedia.org/wiki/X11_color_names
-	void focus_win(Window w){
-		XSetInputFocus(dpy,w,RevertToParent,CurrentTime);
-		XRaiseWindow(dpy,w);
-		XSetWindowBorder(dpy,w,border_color.pixel);
-		XSetWindowBorderWidth(dpy,w,3);
-	}
+	XAllocNamedColor(dpy,DefaultColormap(dpy,DefaultScreen(dpy)),"blue",&focus_border_color,&exact);//color_name at /usr/share/X11/rgb.txt,or you can see https://en.wikipedia.org/wiki/X11_color_names
+	XAllocNamedColor(dpy,DefaultColormap(dpy,DefaultScreen(dpy)),"red",&unfocus_border_color,&exact);
 	while(True){
 		XNextEvent(dpy,&ev);
 		if(ev.type==MapRequest){
 			XSelectInput(dpy, ev.xmaprequest.window, EnterWindowMask);
 			XMapWindow(dpy,ev.xmaprequest.window);
 			//XSetInputFocus(dpy,ev.xfocus.window,RevertToParent,CurrentTime);
-			focus_win(ev.xmaprequest.window);
+			focus_win_func(ev.xmaprequest.window);
 		}
 		else if(ev.type==KeyPress){
 			/*
@@ -60,8 +68,15 @@ int main(){
 				
 			}
 			*/
-			if(ev.xkey.keycode==q_code && ev.xkey.subwindow!=None){
-				XKillClient(dpy,ev.xkey.subwindow);
+			if(ev.xkey.keycode==q_code){
+				//XKillClient(dpy,ev.xkey.subwindow);
+				Window focus = None;
+				int revert;
+				XGetInputFocus(dpy, &focus, &revert);
+				if (focus != None && focus != DefaultRootWindow(dpy)) {
+					XKillClient(dpy, focus);
+				}
+				
 			}
 		}
 		else if(ev.type==ButtonPress && ev.xbutton.subwindow!=None){
@@ -71,8 +86,8 @@ int main(){
 			mc_origin_y=ev.xbutton.y_root;
 		}
 		else if(ev.type==MotionNotify && start.subwindow!=None){
-			int moved_x=ev.xbutton.x_root;//滑鼠移動後的xy
-			int moved_y=ev.xbutton.y_root;
+			int moved_x=ev.xmotion.x_root;//滑鼠移動後的xy
+			int moved_y=ev.xmotion.y_root;
 
 			int move_x=moved_x-mc_origin_x;//滑鼠了xy移動多少
 			int move_y=moved_y-mc_origin_y;
@@ -96,8 +111,18 @@ int main(){
 		}
 		
 		else if(ev.type==EnterNotify && ev.xcrossing.window!=None){
-			//XRaiseWindow(dpy,ev.xcrossing.subwindow);
-			focus_win(ev.xcrossing.window);
+			Window focus_win=ev.xcrossing.window;
+			if(focus_win==DefaultRootWindow(dpy)){
+				continue;
+			}
+			focus_win_func(focus_win);
+
+			
+		}
+		else if (ev.type == DestroyNotify) {
+		    if (ev.xdestroywindow.window == focused_win) {
+		        focused_win = None;
+		    }
 		}
 		XFlush(dpy);
 	}
